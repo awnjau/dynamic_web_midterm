9869f2f573fec5606ba4fc6e4de1b72fc9904ff8
earth2milo <90530636+earth2milo@users.noreply.github.com>
2023-10-30 22:02:47 -0400

    updated

---
+++ src/pages/index.js
@@ -0,0 +1,11 @@
+import "../app/globals.css";
+import CountryFlag from "../app/Components/CountryFlag";
+
+function App() {
+    return (
+    <div>
+      <CountryFlag />
+    </div>
+  );
+};
+export default App;
